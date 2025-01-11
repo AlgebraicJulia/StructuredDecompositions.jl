764a8701e792639ca46300b30710f5eb4e450f55
Richard Samuelson <samuelson.rick@gmail.com>
2025-01-11 00:11:09 -0500

    fixed script

--- makebenchmarks.jl
+++ makebenchmarks.jl
@@ -3,45 +3,51 @@ using PkgBenchmark
 using PkgBenchmark: benchmarkgroup
 using StructuredDecompositions
 
-group = benchmarkgroup(benchmarkpkg(StructuredDecompositions))
-markdown = "# Benchmarks\n\n"
 
-for colors in (2, 3)
-    markdown *= "## $colors Coloring\n\n"
-    markdown *= "| library | bags | time | gctime | memory | allocs |\n"
-    markdown *= "| :------ | :--- | :----| :----- | :----- | :----- |\n"
+function makebenchmarks(name)
+    group = benchmarkgroup(benchmarkpkg(StructuredDecompositions))
+    markdown = "# Benchmarks\n\n"
 
-    trial = group["graph coloring fixed"]["$colors coloring"]["StructuredDecompositions"]["1 bag"]
-    estimate = minimum(trial)
-    markdown *= "| StructuredDecompositions | 1 | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
+    for colors in (2, 3)
+        markdown *= "## $colors Coloring\n\n"
+        markdown *= "| library | bags | time | gctime | memory | allocs |\n"
+        markdown *= "| :------ | :--- | :----| :----- | :----- | :----- |\n"
 
-    for bags in (2, 4, 8, 12)
-        trial = group["graph coloring fixed"]["$colors coloring"]["StructuredDecompositions"]["$bags bags"]
+        trial = group["graph coloring fixed"]["$colors coloring"]["StructuredDecompositions"]["1 bag"]
         estimate = minimum(trial)
-        markdown *= "| StructuredDecompositions | $bags | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
+        markdown *= "| StructuredDecompositions | 1 | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
+
+        for bags in (2, 4, 8, 12)
+            trial = group["graph coloring fixed"]["$colors coloring"]["StructuredDecompositions"]["$bags bags"]
+            estimate = minimum(trial)
+            markdown *= "| StructuredDecompositions | $bags | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
+        end
+
+        trial = group["graph coloring fixed"]["$colors coloring"]["HomSearch"]
+        estimate = minimum(trial)
+        markdown *= "| HomSearch |  | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n\n"
     end
 
-    trial = group["graph coloring fixed"]["$colors coloring"]["HomSearch"]
-    estimate = minimum(trial)
-    markdown *= "| HomSearch |  | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n\n"
-end
 
+    for colors in (4,)
+        markdown *= "## $colors Coloring\n\n"
+        markdown *= "| library | bags | time | gctime | memory | allocs |\n"
+        markdown *= "| :------ | :--- | :--- | :----- | :----- | :----- |\n"
+        
+        for bags in (4, 8, 12)
+            trial = group["graph coloring fixed"]["$colors coloring"]["StructuredDecompositions"]["$bags bags"]
+            estimate = minimum(trial)
+            markdown *= "| StructuredDecompositions | $bags | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
+        end
 
-for colors in (4,)
-    markdown *= "## $colors Coloring\n\n"
-    markdown *= "| library | bags | time | gctime | memory | allocs |\n"
-    markdown *= "| :------ | :--- | :--- | :----- | :----- | :----- |\n"
-    
-    for bags in (4, 8, 12)
-        trial = group["graph coloring fixed"]["$colors coloring"]["StructuredDecompositions"]["$bags bags"]
+        trial = group["graph coloring fixed"]["4 coloring"]["HomSearch"]
         estimate = minimum(trial)
-        markdown *= "| StructuredDecompositions | $bags | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
+        markdown *= "| HomSearch |  | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
     end
 
-    trial = group["graph coloring fixed"]["4 coloring"]["HomSearch"]
-    estimate = minimum(trial)
-    markdown *= "| HomSearch |  | $(prettytime(estimate.time)) | $(prettytime(estimate.gctime)) | $(prettymemory(estimate.memory)) | $(estimate.allocs) |\n"
+    write(name, markdown)
 end
+
 
 name = isempty(ARGS) ? "bencharks.md" : first(ARGS)
-write(name, markdown)
+makebenchmarks(name)
