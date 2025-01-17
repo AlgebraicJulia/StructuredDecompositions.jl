6201b36ab6ee3df738b2bccaaf4c6aec0905d4a7
Richard Samuelson <samuelson.rick@gmail.com>
2025-01-17 12:00:01 -0500

    testing faster homomorphism function

--- test/DecidingSheaves.jl
+++ test/DecidingSheaves.jl
@@ -1,17 +1,22 @@
 module TestDecidingSheaves
 
-using Test
-using PartialFunctions
-using MLStyle
 
+using Catlab
+using MLStyle
+using PartialFunctions
 using StructuredDecompositions.Decompositions
 using StructuredDecompositions.DecidingSheaves
 using StructuredDecompositions.FunctorUtils
+using Test
 
-using Catlab.Graphs
-using Catlab.ACSetInterface
-using Catlab.CategoricalAlgebra
-using Catlab.Graphics
+
+# fixing bug upstream
+function Catlab.WiringDiagramAlgebras.make_homomorphism(row, X::StructACSet{S}, Y::StructACSet{S}) where S
+  components = let i = 0
+    NamedTuple{ob(S)}(Int[row[i+=1] for _ in parts(X,c)] for c in ob(S))
+  end
+  ACSetTransformation(components, X, Y)
+end
 
 
 function K(n::Integer)
@@ -25,7 +30,7 @@ end
 
 
 function (coloring::Coloring)(graph::Graph)
-    FinSet(homomorphisms(graph, K(coloring.n)))
+    FinSet(homomorphisms(graph, K(coloring.n); alg=HomomorphismQuery()))
 end
 
 
